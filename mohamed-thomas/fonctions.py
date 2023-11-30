9b14b9b740e075191f525683d4d5e0ba03a327f6
ThomasRAYNAUD <toto.raynaud@sfr.fr>
2023-11-30 15:52:38 +0100

    additionFonction

---
+++ mohamed-thomas/fonctions.py
@@ -0,0 +1,24 @@
+def addition(x, y):
+     result = x+y
+    return result
+
+
+def soustraction(x, y):
+    """Retourne la différence de x et y"""
+    return 
+
+
+def noms_binome():
+    """
+    Affiche les noms des membres du binôme
+    Attention, chacun écrit la ligne pour afficher son nom
+    """
+
+
+a = 2
+b = 1
+
+print(f"La somme de {a} et {b} vaut {addition(a, b)}")
+print(f"La différence de {a} et {b} vaut {soustraction(a, b)}")
+
+noms_binome()
